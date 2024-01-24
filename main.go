c2d07393cfde3244fdecc9630ad02d86bee07c41
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2024-01-24 23:21:09 +0700

    feat: create `handler` function for user and set route for handler

--- main.go
+++ main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"startup-gin/user"
 
+	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
+	router := gin.Default()
+	router.GET("/handler", handler)
+
+	router.Run()
+}
+
+func handler(c *gin.Context) {
 	dsn := "root:root@tcp(127.0.0.1:3306)/startup_db?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
@@ -22,9 +31,5 @@ func main() {
 	var users []user.User
 	db.Find(&users)
 
-	for _, user := range users {
-		fmt.Println(user.Name)
-		fmt.Println(user.Email)
-		fmt.Println("=====")
-	}
+	c.JSON(http.StatusOK, users)
 }
