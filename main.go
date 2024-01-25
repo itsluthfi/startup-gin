67bcd84d4e8651ebc0baa9f87bdecc1dffe2c168
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2024-01-25 19:48:24 +0700

    feat(user): create repository and `Save` function for user

--- main.go
+++ main.go
@@ -1,24 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
 	"startup-gin/user"
 
-	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
-	router := gin.Default()
-	router.GET("/handler", handler)
-
-	router.Run()
-}
-
-func handler(c *gin.Context) {
 	dsn := "root:root@tcp(127.0.0.1:3306)/startup_db?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
@@ -26,10 +16,10 @@ func handler(c *gin.Context) {
 		log.Fatal(err.Error())
 	}
 
-	fmt.Println("Connection to database is good")
+	userRepository := user.NewRepository(db)
+	user := user.User{
+		Name: "Hanif",
+	}
 
-	var users []user.User
-	db.Find(&users)
-
-	c.JSON(http.StatusOK, users)
+	userRepository.Save(user)
 }
