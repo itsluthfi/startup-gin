52452098076ad61c75c155e796c2a5732b268450
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2024-02-19 14:31:06 +0700

    feat(campaign): create `CreateCampaign` service for campaign
    This commit also creates Campaign input.

--- campaign/service.go
+++ campaign/service.go
@@ -1,6 +1,13 @@
 package campaign
 
+import (
+	"fmt"
+
+	"github.com/gosimple/slug"
+)
+
 type Service interface {
+	CreateCampaign(input CreateCampaignInput) (Campaign, error)
 	GetCampaigns(userID int) ([]Campaign, error)
 	GetCampaignByID(input GetCampaignDetailInput) (Campaign, error)
 }
@@ -11,6 +18,26 @@ type service struct {
 
 func NewService(repository Repository) *service {
 	return &service{repository}
+}
+
+func (s *service) CreateCampaign(input CreateCampaignInput) (Campaign, error) {
+	campaign := Campaign{}
+	campaign.Name = input.Name
+	campaign.ShortDescription = input.ShortDescription
+	campaign.Description = input.Description
+	campaign.GoalAmount = input.GoalAmount
+	campaign.Perks = input.Perks
+	campaign.UserID = input.User.ID
+
+	slugCandidate := fmt.Sprintf("%s %d", input.Name, input.User.ID)
+	campaign.Slug = slug.Make(slugCandidate)
+
+	newCampaign, err := s.repository.Save(campaign)
+	if err != nil {
+		return newCampaign, err
+	}
+
+	return newCampaign, nil
 }
 
 func (s *service) GetCampaigns(userID int) ([]Campaign, error) {
