7859676f4a669232c2aeac375d7ad2cb52e6f6d2
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2024-02-20 15:12:34 +0700

    feat(campaign): create `Update` repository for campaign

--- campaign/repository.go
+++ campaign/repository.go
@@ -7,6 +7,7 @@ type Repository interface {
 	FindAll() ([]Campaign, error)
 	FindByUserID(userID int) ([]Campaign, error)
 	FindByID(ID int) (Campaign, error)
+	Update(campaign Campaign) (Campaign, error)
 }
 
 type repository struct {
@@ -52,6 +53,15 @@ func (r *repository) FindByID(ID int) (Campaign, error) {
 	var campaign Campaign
 
 	err := r.db.Preload("User").Preload("CampaignImages").Where("id = ?", ID).Find(&campaign).Error
+	if err != nil {
+		return campaign, err
+	}
+
+	return campaign, nil
+}
+
+func (r *repository) Update(campaign Campaign) (Campaign, error) {
+	err := r.db.Save(&campaign).Error
 	if err != nil {
 		return campaign, err
 	}
