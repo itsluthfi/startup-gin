32e7095639fa95f403fc0d69c3d1e472941db418
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2024-02-26 18:50:47 +0700

    feat(transaction): create `GetTransactionsByCampaignID` service for transaction

--- transaction/service.go
+++ transaction/service.go
@@ -1,1 +1,37 @@
 package transaction
+
+import (
+	"errors"
+	"startup-gin/campaign"
+)
+
+type Service interface {
+	GetTransactionsByCampaignID(input GetCampaignTransactionsInput) ([]Transaction, error)
+}
+
+type service struct {
+	repository         Repository
+	campaignRepository campaign.Repository
+}
+
+func NewService(repository Repository, campaignRepository campaign.Repository) *service {
+	return &service{repository, campaignRepository}
+}
+
+func (s *service) GetTransactionsByCampaignID(input GetCampaignTransactionsInput) ([]Transaction, error) {
+	campaign, err := s.campaignRepository.FindByID(input.ID)
+	if err != nil {
+		return []Transaction{}, err
+	}
+
+	if campaign.UserID != input.User.ID {
+		return []Transaction{}, errors.New("Not an owner of campaign")
+	}
+
+	transactions, err := s.repository.GetCampaignByID(input.ID)
+	if err != nil {
+		return transactions, err
+	}
+
+	return transactions, nil
+}
