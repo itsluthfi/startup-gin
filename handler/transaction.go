c6beec358e58a8c2d8b8a96a2c3086466284bdb8
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2024-02-28 19:21:17 +0700

    feat(handler): create `CreateTransaction` handler for transaction

--- handler/transaction.go
+++ handler/transaction.go
@@ -17,6 +17,35 @@ func NewTransactionHandler(transactionService transaction.Service) *transactionH
 	return &transactionHandler{transactionService}
 }
 
+func (h *transactionHandler) CreateTransaction(c *gin.Context) {
+	var input transaction.CreateTransactionInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Failed to create transaction", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	currentUser := c.MustGet("currentUser").(user.User)
+
+	input.User = currentUser
+
+	newTransaction, err := h.transactionService.CreateTransaction(input)
+	if err != nil {
+		response := helper.APIResponse("Failed to create transaction", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("Success to create transaction", http.StatusCreated, "success", transaction.FormatTransaction(newTransaction))
+
+	c.JSON(http.StatusCreated, response)
+}
+
 func (h *transactionHandler) GetCampaignTransactions(c *gin.Context) {
 	var input transaction.GetCampaignTransactionsInput
 
