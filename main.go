9bbf908726ff0557f28a7d1c990ed67783d073f1
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2024-02-29 23:49:21 +0700

    build: install CORS middleware

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"startup-gin/user"
 	"strings"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 	"gorm.io/driver/mysql"
@@ -39,6 +40,7 @@ func main() {
 	transactionHandler := handler.NewTransactionHandler(transactionService)
 
 	router := gin.Default()
+	router.Use(cors.Default())
 	router.Static("/images", "./images")
 	api := router.Group("api/v1")
 
