f496d368f3a4c5e7f11c08c0b050b45f8cbf4ee7
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2024-02-29 23:32:14 +0700

    feat(transaction): create `ProcessPayment` service for transaction
    This commit also creates Transaction notification input.

--- transaction/input.go
+++ transaction/input.go
@@ -12,3 +12,10 @@ type GetCampaignTransactionsInput struct {
 	ID   int `uri:"id" binding:"required"`
 	User user.User
 }
+
+type TransactionNotificationInput struct {
+	TransactionStatus string `json:"transaction_status"`
+	OrderID           string `json:"order_id"`
+	PaymentType       string `json:"payment_type"`
+	FraudStatus       string `json:"fraud_status"`
+}
--- transaction/service.go
+++ transaction/service.go
@@ -3,12 +3,14 @@ package transaction
 import (
 	"errors"
 	"startup-gin/campaign"
+	"strconv"
 )
 
 type Service interface {
 	CreateTransaction(input CreateTransactionInput) (Transaction, error)
 	GetTransactionsByCampaignID(input GetCampaignTransactionsInput) ([]Transaction, error)
 	GetTransactionsByUserID(userID int) ([]Transaction, error)
+	ProcessPayment(input TransactionNotificationInput) error
 }
 
 type service struct {
@@ -60,4 +62,43 @@ func (s *service) GetTransactionsByUserID(userID int) ([]Transaction, error) {
 	}
 
 	return transactions, nil
+}
+
+func (s *service) ProcessPayment(input TransactionNotificationInput) error {
+	transaction_id, _ := strconv.Atoi(input.OrderID)
+
+	transaction, err := s.repository.GetByID(transaction_id)
+	if err != nil {
+		return err
+	}
+
+	if input.PaymentType == "credit_card" && input.TransactionStatus == "capture" && input.FraudStatus == "accept" {
+		transaction.Status = "paid"
+	} else if input.TransactionStatus == "settlement" {
+		transaction.Status = "paid"
+	} else if input.TransactionStatus == "deny" || input.TransactionStatus == "expire" || input.TransactionStatus == "cancel" {
+		transaction.Status = "cancelled"
+	}
+
+	updatedTransaction, err := s.repository.Update(transaction)
+	if err != nil {
+		return err
+	}
+
+	campaign, err := s.campaignRepository.FindByID(updatedTransaction.CampaignID)
+	if err != nil {
+		return err
+	}
+
+	if updatedTransaction.Status == "paid" {
+		campaign.BackerCount = campaign.BackerCount + 1
+		campaign.CurrentAmount = campaign.CurrentAmount + updatedTransaction.Amount
+
+		_, err := s.campaignRepository.Update(campaign)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
